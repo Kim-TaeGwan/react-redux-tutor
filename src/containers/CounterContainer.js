ab87ffd186e15eab57d582d2a9b738dcac2bcda2
kimtaegwan <ab154@naver.com>
2022-06-01 02:11:51 +0900

    Add dispatch v4

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,7 +1,6 @@
 import { connect } from "react-redux";
 import Counter from "../components/Counter";
 import { decrease, increase } from "../modules/counter";
-import { bindActionCreators } from "redux";
 
 // 리덕스 스토어와 연동된 컴포넌트를 컨테이너 컴포넌트라고 부른다.
 const CounterContainer = ({ number, increase, decrease }) => {
@@ -10,25 +9,15 @@ const CounterContainer = ({ number, increase, decrease }) => {
   );
 };
 
-// 컴포넌트를 리덕스와 연동하려면 아래 코드 처럼 react-redux 에서 제공하는 connect 함수를 사용해야힌다.
-// connect(mapStateToProps, mapDispatchToProps)(연동할 컴포넌트)
-// mapStateToProps 는 리덕스 스토어 안의 상태를 컴포넌트의 props 로 넘겨주기 위해 설정하는 함수.
-// mapDispatchToProps 는 액션 생성 함수를 컴포넌트의 props 로 넘겨주기 위 사용하는 함수
+// mapDIspatchToProps에 해당하는 파라미터를 함수형태가 아닌 액션 생성 함수로 이루어진 객체 형태로 넣어준다.
+// 아래와 같이 두번째 파라미터를 아예 객체 형태로 넣어주면 connect 함수가 내부적으로 bindActionCreators 작업을 대신해 준다.
 
-// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
 export default connect(
   (state) => ({
     number: state.counter.number,
   }),
-  (dispatch) =>
-    bindActionCreators(
-      {
-        increase,
-        decrease,
-      },
-      dispatch
-    )
+  {
+    increase,
+    decrease,
+  }
 )(CounterContainer);
-// mapStateToProps 와 mapDispatchToProps 에서 반환하는 객체 내부의 값들은 컴포넌트의 props 로 전달된다.
-// mapStateToProps 는 state 를 파라미터로 받아 오며, 이 값은 현재 스토어가 지니고 있는 상태를 가리킨다.
-// mapDispatchToProps 의 경우 store 의 내장 함수 dispatch 를 파라미터로 받아온다.
